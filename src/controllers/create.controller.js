34fcf56ec470e006cef3b8f3ddaff387a46434e8
Nelson Morales <camilomorales2615@gmail.com>
2023-10-31 13:55:43 -0500

    Format

--- src/controllers/create.controller.js
+++ src/controllers/create.controller.js
@@ -16,24 +16,38 @@ export const adduser = async (req, res) => {
 
   // Validations
   if (data.password.trim() !== data.validate_password.trim()) {
-    res.status(401).json({ error: 'Passwords are not equals' });
+    res.status(401).json({ error: "Passwords are not equals" });
     return;
   }
 
   if ((await existuser(data.user.trim())) === true) {
-    res.status(401).json({ error: 'User already exists' });
+    res.status(401).json({ error: "User already exists" });
     return;
   }
 
-  let encrypted_pass = cryppass(data.password.trim())
+  let encrypted_pass = cryppass(data.password.trim());
   try {
-
-    if (data.user.trim() === '' || data.password.trim() === '' || data.first_name.trim() === '' || data.last_name.trim() === '' || data.email.trim() === '' || data.phone.trim() === '' || data.validate_password.trim() === '') {
-        return res.status(401).json({error:'There are blank characters'})
+    if (
+      data.user.trim() === "" ||
+      data.password.trim() === "" ||
+      data.first_name.trim() === "" ||
+      data.last_name.trim() === "" ||
+      data.email.trim() === "" ||
+      data.phone.trim() === "" ||
+      data.validate_password.trim() === ""
+    ) {
+      return res.status(401).json({ error: "There are blank characters" });
     } else {
       const [rows] = await pool.query(
         "INSERT INTO user_data (username, password, first_name, last_name, phone, email)  VALUES (?,?,?,?,?,?)",
-        [data.user.trim(), encrypted_pass, data.first_name.trim(), data.last_name.trim(), data.phone.trim(), data.email.trim()]
+        [
+          data.user.trim(),
+          encrypted_pass,
+          data.first_name.trim(),
+          data.last_name.trim(),
+          data.phone.trim(),
+          data.email.trim(),
+        ]
       );
       res.json({ redirect: "/" });
     }
